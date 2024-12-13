ea7f696b04d1b11145724bbb38a8f87ab1779970
FaithAgnes <agyfaith12katushabe@gmail.com>
2024-12-13 07:42:07 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify, send_file
 import os
+import nltk
 from mymodule import plagiarism_checker, visualize_heatmap
+
+# necessary NLTK data files are downloaded
+nltk.download('punkt') 
 
 app = Flask(__name__)
 
-DOCUMENTS_DIR = '/app/documents'  # This will be the mounted directory
+DOCUMENTS_DIR = '/app/documents'
 
 @app.route('/favicon.ico')
 def favicon():
-    return '', 204  # Serve an empty favicon
+    return '', 204
 
 @app.route('/')
 def home():
@@ -22,32 +26,22 @@ def check_plagiarism():
     doc1 = request.files['doc1']
     doc2 = request.files['doc2']
 
-    # Save uploaded files to a temporary location within the mounted directory
     doc1_path = os.path.join(DOCUMENTS_DIR, doc1.filename)
     doc2_path = os.path.join(DOCUMENTS_DIR, doc2.filename)
     doc1.save(doc1_path)
     doc2.save(doc2_path)
 
     try:
-        # Call plagiarism checker function
         score, heatmap_data = plagiarism_checker(doc1_path, doc2_path)
 
-        # Save the heatmap as an image
         save_path = "/tmp/heatmap.png"
         if heatmap_data:
             visualize_heatmap(heatmap_data, doc1_name=doc1.filename, doc2_name=doc2.filename, save_path=save_path)
 
-        # Return result with score and heatmap
         return jsonify({'score': score, 'heatmap_url': '/download_heatmap'}), 200
 
     except Exception as e:
-        # Handle exceptions and return error response
         return jsonify({'error': str(e)}), 500
-
-    # Remove the cleanup code (if you still want to clear up occasionally, consider manual or scheduled cleanups)
-    # finally:
-    #     os.remove(doc1_path)
-    #     os.remove(doc2_path)
 
 @app.route('/download_heatmap', methods=['GET'])
 def download_heatmap():
@@ -55,3 +49,4 @@ def download_heatmap():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
